7a852a5ef76885dbc256f59136855e0f3556dbfc
free <free@example.com>
2025-02-15 14:01:43 -0500

    lesson#3-1:Asteroids

--- main.py
+++ main.py
@@ -4,23 +4,36 @@
 import pygame
 from pygame.locals import *
 from constants import *
+from player import Player
 
 
 def main():
     pygame.init()
     print("Starting asteroids!")
-    #print(f"Screen width: {SCREEN_WIDTH}")
-    #print(f"Screen height: {SCREEN_HEIGHT}")
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    loop(screen)
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    
+    loop(screen, updatable, drawable)
    
-def loop(screen):
+def loop(screen, updatable, drawable):
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        for d in drawable:
+            d.draw(screen)
+        #drawable.draw(screen)
+        
         pygame.display.flip()
+        dt = (clock.tick(60) / 1000)
+        updatable.update(dt)
 
 if __name__ == "__main__":
     main()
